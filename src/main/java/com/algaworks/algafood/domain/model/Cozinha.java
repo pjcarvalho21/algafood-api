b7204460e6e7a5383d8dc1d968f18986ca202503
Paulo J. Carvalho <pjcarvalho21@gmail.com>
2022-03-15 12:56:14 -0300

    resolvendo conflito

--- src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
+++ src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
@@ -2,30 +2,6 @@ package com.algaworks.algafood.domain.model;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-<<<<<<< HEAD
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-
-import com.fasterxml.jackson.annotation.JsonRootName;
-
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-
-@JsonRootName("cozinha")
-@Data
-@EqualsAndHashCode(onlyExplicitlyIncluded = true)
-@Entity
-public class Cozinha {
-
-	@EqualsAndHashCode.Include
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
-	
-	@Column(nullable = false)
-	private String nome;
-=======
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -79,6 +55,6 @@ public class Cozinha {
 			return false;
 		return true;
 	}
->>>>>>> branch 'desenv' of https://github.com/pjcarvalho21/algafood-api.git
+
 	
 }
